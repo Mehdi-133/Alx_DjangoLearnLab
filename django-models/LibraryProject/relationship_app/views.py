00545b6e6218145d2498fa22173fdc5a32a1b2e6
Mehdi-133 <mehdikarbitou@gmail.com>
2024-11-21 15:17:31 +0100

    add the urls and the view with templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.all()  # Fetch all books
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+
