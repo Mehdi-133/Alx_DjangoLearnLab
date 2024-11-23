71a1d2e806fe4f39c26885c21ff1ddcc87f88ade
Mehdi-133 <mehdikarbitou@gmail.com>
2024-11-23 18:35:53 +0100

    update admin file

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -8,3 +8,30 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
 
 admin.site.register(Book,BookAdmin)
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    """
+    Custom admin interface for the CustomUser model.
+    """
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (  # Add custom fields to the existing fieldsets
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (  # Add custom fields to the add user form
+        ('Additional Info', {
+            'classes': ('wide',),
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']  # Customize the admin list view
+    search_fields = ['username', 'email']  # Add search capabilities for username and email
+
+# Register the CustomUser model with the custom admin class
+admin.site.register(CustomUser, CustomUserAdmin)
