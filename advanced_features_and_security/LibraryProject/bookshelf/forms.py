3994c55fa7cc62b6231cca8377102963f67441c5
Mehdi-133 <mehdikarbitou@gmail.com>
2024-11-23 19:39:17 +0100

    update view file

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -3,3 +3,9 @@ from django import forms
 
 class BookSearchForm(forms.Form):
     search_query = forms.CharField(max_length=255)
+
+
+from django import forms
+
+class ExampleForm(forms.Form):
+    search_query = forms.CharField(max_length=255, required=True, label="Search Query")
