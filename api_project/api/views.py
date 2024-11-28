13c8883cce3f3e862b06c440f00c5456dbed3348
Mehdi-133 <mehdikarbitou@gmail.com>
2024-11-28 10:25:59 +0100

    modify views file

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
